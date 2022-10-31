d3e134098ce8b21a4f3f508b2e43a2cdf0e55e3b
bdls-jamal <kabsschmals@outlook.com>
2022-10-31 13:58:15 -0400

    Kobi: Added changes to the SQLManager to create a table

--- src/main/java/SQLManager.java
+++ src/main/java/SQLManager.java
@@ -3,8 +3,20 @@ import java.sql.*;
 public class SQLManager {
     //tablename = UserInformation ?
     //    ajfiojsaidoiajfs
-    Connection database = DriverManager.getConnection();
-    public static void viewTable(Connection con) throws SQLException {
+    static Connection database;
+
+    static {
+        try {
+            database = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "root");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public SQLManager() throws SQLException {
+    }
+
+    public static boolean viewTable(Connection con) throws SQLException {
         String query = "select ID, USERNAME, PASSWORD, EMAIL, BODYFAT, WEIGHT from USERINFORMATION";
         try (Statement stmt = con.createStatement()) {
             ResultSet rs = stmt.executeQuery(query);
@@ -21,9 +33,10 @@ public class SQLManager {
         } catch (SQLException e) {
             //SQL Exception
         }
+        return false;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println(viewTable(database));
     }
 }
