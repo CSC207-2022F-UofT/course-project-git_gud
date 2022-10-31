527c016e443b75af9a33ab41a4d620d28b6aba44
bdls-jamal <kabsschmals@outlook.com>
2022-10-31 13:14:20 -0400

    Kobi: Added DashboardController and presenter

---
+++ src/main/DashboardController.java
@@ -0,0 +1,35 @@
+public class SettingsUseCaseController {
+
+    public LogOutUseCase{
+        //logout
+        //return to login screen(Adrian's work)
+    }
+
+    public DeleteAccountUseCase{
+        //remove account data from database(username, associated password/email, associated stats)
+    }
+
+    public ChangeEmailUseCase{
+
+    }
+
+    public ChangePasswordUseCase{
+
+    }
+
+    public ChangeUsernameUseCase{
+
+    }
+
+    public ChangeWeightBodyFatUseCase{
+
+    }
+
+    /*public ChangeLanguageUseCase{
+
+    }*/
+
+    public UpdateView{
+        // call presenter
+    }
+}
