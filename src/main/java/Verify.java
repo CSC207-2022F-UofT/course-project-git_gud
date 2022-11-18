d3a70a5fac80c310ab3413b7565c2ee9edcb80c3
adrianhui <adrianhssc@gmail.com>
2022-11-18 17:50:47 -0500

    sns

---
+++ src/main/java/Verify.java
@@ -0,0 +1,11 @@
+import java.awt.event.ActionEvent;
+
+public class Verify {
+
+    public static void Exist(LoginPage page){
+        String username = page.usernameField.getText();
+        String password = String.valueOf(page.passwordField.getPassword());
+
+
+    }
+}
