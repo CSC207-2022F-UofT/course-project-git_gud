1d0edb680344d311174ff2299143bc3d1fe44369
adrianhui <adrianhssc@gmail.com>
2022-12-01 18:17:44 -0500

    Trying back buttom on routine

--- src/main/java/views/RoutineManagerView.java
+++ src/main/java/views/RoutineManagerView.java
@@ -5,12 +5,12 @@ import management.RoutineManager;
 
 public class RoutineManagerView {
 
-    public static void main(String[] args) {
+    public RoutineManagerView() {
         WorkoutSelectionView theView = new WorkoutSelectionView();
 
         RoutineManager theModel = new RoutineManager();
 
         new WorkoutSelectionController(theModel, theView);
+
     }
 }
-
