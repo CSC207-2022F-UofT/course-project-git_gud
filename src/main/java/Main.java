a40657f694a57980e26d573e9bfb2e5b3cb32174
tejcapildeo <tjkool500@gmail.com>
2022-12-01 18:08:48 -0500

    Final commits before merging. Fixed some errors that were occuring during merge conflicts.

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import views.ViewLoginPage;
-public class Main {
-    /**
-     * This main class instantiates the UI
-     */
-    public static void main(String[] args) {ViewLoginPage login = new ViewLoginPage();
-    }
-}
