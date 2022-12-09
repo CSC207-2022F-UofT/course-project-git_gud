223ab8fc8a6f5148e97aa275359776aa747ff417
turne142 <jake.turner@mail.utoronto.ca>
2022-12-08 19:51:38 -0500

    Added unit tests to all classes in management.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 //import views.ViewLoginPage;
 import views.dashboard.SettingsPage;
 
+import java.sql.SQLException;
+
 public class Main{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         SettingsPage settings = new SettingsPage();
         settings.CreateSettingsPage(null);
     }
